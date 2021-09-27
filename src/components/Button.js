170e9ca337adf673c08f559bdf6c93979c29a2a7
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-09-28 00:16:49 +0530

    deleting a file

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,6 +6,13 @@ const STYLES = ['btn--primary', 'btn--outline'];
 const SIZES = ['btn--medium', 'btn--large'];
 
 export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {   
- const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
- const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+ const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+ const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+ return(
+     <Link to='/sign-up' className='btn-mobile'>
+         <button className={`btn ${checkButtoonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
+             {children}
+         </button>
+     </Link>
+ )
 } 
