4fe6dab103baf3c8eac608eafea7529c0c2eb69b
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-10-05 22:29:13 +0530

    new changes

---
+++ src/components/Navbar/newfile.js
@@ -0,0 +1,10 @@
+function Reverse (number) {
+    var newNum = 0;
+    for(var i =number.legth -1; i>=0; i--){
+        newNum = newNum + number[i];
+        return new
+    }
+}
+
+var number = 4568;
+console.log(Reverse(number));
